f3b9cf51dedb0396c63bd568c72b28af61de7201
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-01-09 19:13:50 -0300

    refactor: remove express auth

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import { ExpressAuth } from '@auth/express'
-import Credentials from '@auth/express/providers/credentials'
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
@@ -7,17 +5,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  const expressApp = app.getHttpAdapter().getInstance()
-
   app.useGlobalPipes(new ValidationPipe())
-  expressApp.set('trust proxy', true)
-  app.use(
-    '/auth/*',
-    ExpressAuth({
-      providers: [Credentials({})],
-    }),
-  )
-
   await app.listen(process.env.PORT ?? 8080)
 }
 bootstrap()
