38a9cbd9e0fc8f58b5f1110919de63dd0b7725c5
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-01-08 21:14:28 -0300

    feat: add auth.js with initial config

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { ExpressAuth } from '@auth/express'
+import Credentials from '@auth/express/providers/credentials'
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
@@ -5,8 +7,16 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const expressApp = app.getHttpAdapter().getInstance()
 
   app.useGlobalPipes(new ValidationPipe())
+  expressApp.set('trust proxy', true)
+  app.use(
+    '/auth/*',
+    ExpressAuth({
+      providers: [Credentials({})],
+    }),
+  )
 
   await app.listen(process.env.PORT ?? 8080)
 }
