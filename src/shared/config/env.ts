d8c7ac6ec3e90b40f4860707ab6c953bdfaf2725
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-02-04 22:45:06 -0300

    chore: add news environment variables in env.exemple file and env config

---
+++ src/shared/config/env.ts
@@ -0,0 +1,38 @@
+import { plainToInstance } from 'class-transformer'
+import { IsNotEmpty, IsString, validateSync } from 'class-validator'
+
+class Env {
+  @IsString()
+  @IsNotEmpty()
+  databaseUrl: string
+
+  @IsString()
+  @IsNotEmpty()
+  dbUser: string
+
+  @IsString()
+  @IsNotEmpty()
+  dbPassword: string
+
+  @IsString()
+  @IsNotEmpty()
+  dbPort: string
+
+  @IsString()
+  @IsNotEmpty()
+  dbName: string
+}
+
+export const env: Env = plainToInstance(Env, {
+  databaseUrl: process.env.DATABASE_URL,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbPort: process.env.DB_PORT,
+  dbName: process.env.DB_NAME,
+})
+
+const errors = validateSync(env)
+
+if (errors.length > 0) {
+  throw new Error(JSON.stringify(errors, null, 2))
+}
