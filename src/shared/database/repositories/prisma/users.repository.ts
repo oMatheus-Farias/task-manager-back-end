ed636f1930129e08bc07e6063c4feaf086e89a05
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-02-05 21:00:40 -0300

    refactor: add pg queries in create and find by id routes

--- src/shared/database/repositories/prisma/users.repository.ts
+++ src/shared/database/repositories/prisma/users.repository.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@nestjs/common'
 import { Prisma, User } from '@prisma/client'
 
+import { PgService } from '../../pg.service'
 import { PrismaService } from '../../prisma.service'
 import { UsersRepository } from '../interfaces/users.repository'
 
 @Injectable()
 export class UsersPrismaRepository implements UsersRepository {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly pgService: PgService,
+  ) {}
 
   async findById(
     userId: string,
   ): Promise<Pick<User, 'id' | 'name' | 'email'> | null> {
-    return await this.prismaService.user.findUnique({
-      where: { id: userId },
-      select: { id: true, name: true, email: true },
-    })
+    const result: Pick<User, 'id' | 'name' | 'email'>[] =
+      await this.pgService.query(
+        `
+      SELECT id, name, email
+      FROM users
+      WHERE id = $1
+    `,
+        [userId],
+      )
+    return result[0] || null
   }
 
   async findByEmail(email: string): Promise<Pick<User, 'id'> | null> {
@@ -25,9 +35,15 @@ export class UsersPrismaRepository implements UsersRepository {
   }
 
   async create(data: Prisma.UserCreateInput): Promise<Pick<User, 'id'>> {
-    return await this.prismaService.user.create({
-      data,
-      select: { id: true },
-    })
+    const result: Pick<User, 'id'>[] = await this.pgService.query(
+      `
+      INSERT INTO users (name, email, password)
+      VALUES ($1, $2, $3)
+      RETURNING id
+    `,
+      [data.name, data.email, data.password],
+    )
+
+    return result[0]
   }
 }
